d9f16195d4db2acd06b5cfb43f99b3ea9816c9e9
Azzarox <62171309+Azzarox@users.noreply.github.com>
2022-04-20 13:29:34 +0300

    production mode activated

--- Project-WebpackFirebase/webpack.config.js
+++ Project-WebpackFirebase/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     devtool: 'eval-source-map',
     entry: ['./src/app.js', './src/server.js'],
     output: {
