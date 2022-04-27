1a361426d7bdd63531192fd3389bf81d6b1ae2ee
Azzarox <62171309+Azzarox@users.noreply.github.com>
2022-04-27 22:02:32 +0300

    added function for dynamically setting height for .comments div

---
+++ Project-WebpackFirebase/src/utils/resizeCommentsDivHeight.js
@@ -0,0 +1,9 @@
+
+export function setDynamicHeight() {
+    const element = document.querySelector('.details-image')
+    console.log(element);
+    const resizedElementHeight = element.naturalHeight / 2;
+    console.log(resizedElementHeight)
+    const commentsSection = document.querySelector('.right-details .comments');
+    commentsSection.style.height = `${resizedElementHeight}px`;
+}
