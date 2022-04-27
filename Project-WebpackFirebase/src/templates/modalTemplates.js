6f3e12823ef14b4ab48bff97bf83034526959e98
Azzarox <62171309+Azzarox@users.noreply.github.com>
2022-04-27 16:54:08 +0300

    added deleteModal

--- Project-WebpackFirebase/src/templates/modalTemplates.js
+++ Project-WebpackFirebase/src/templates/modalTemplates.js
@@ -1,9 +1,9 @@
 import { html } from 'lit-html';
 
 export const successfullyCreatedModal = () => html`
-    <div id="modal-create" class="modal">
+    <div id="modal-create" class="modal is-active">
         <div class="modal-background"></div>
-        <div class="modal-content">
+        <div class="modal-content opacity-8">
             <div class="modal-content-icon">
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -23,6 +23,42 @@ export const successfullyCreatedModal = () => html`
             <div class="modal-content-text">
                 <h2>Successfully Created</h2>
                 <p>You are being redirected...</p>
+            </div>
+        </div>
+    </div>
+`;
+
+export const deleteModal = () => html`
+    <div id="modal-delete" class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="model-content-icon-delete">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="icon icon-tabler circle-x"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                >
+                    <desc>
+                        Download more icon variants from
+                        https://tabler-icons.io/i/circle-x
+                    </desc>
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <circle cx="12" cy="12" r="9"></circle>
+                    <path d="M10 10l4 4m0 -4l-4 4"></path>
+                </svg>
+            </div>
+            <div class="modal-content-text-delete">
+                <p>Are you sure you want to delete?</p>
+                <p>This action cannot be undone</p>
+                <div class="modal-delete-div-buttons">
+                    <button class="button delete-cancel">Cancel</button>
+                    <button class="button is-danger delete-yes">Yes</button>
+                </div>
             </div>
         </div>
     </div>
