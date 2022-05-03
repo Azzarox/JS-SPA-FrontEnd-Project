cb5080f3ca102baa6da683736cc0a3a8123f0c38
Azzarox <62171309+Azzarox@users.noreply.github.com>
2022-05-03 12:10:10 +0300

    added weaker regex and less strict?

--- Project-WebpackFirebase/src/utils/imageUrlValidator.js
+++ Project-WebpackFirebase/src/utils/imageUrlValidator.js
@@ -1,4 +1,7 @@
-const regexPhoto = /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/gim;
+// const regexPhoto = /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/gim;
+
+// NOTE: Less strict regex? 
+const regexPhoto = /(https?:\/\/.*\.(?:png|jpg))/i 
 
 export function imageURLIsNotCorrectValidator(imageField) {
     return !regexPhoto.test(imageField);
