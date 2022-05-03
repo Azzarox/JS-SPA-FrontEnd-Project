68b4f3ec0addb808a0d88e6e0defcb7edb381a8d
Azzarox <62171309+Azzarox@users.noreply.github.com>
2022-05-03 13:49:45 +0300

    ordered comments by timestamp

--- Project-WebpackFirebase/src/middlewares/detailsPreloader.js
+++ Project-WebpackFirebase/src/middlewares/detailsPreloader.js
@@ -1,4 +1,11 @@
-import { getByQuery, getCollectionReference, getSingleDoc } from '../server';
+import { orderBy, query } from 'firebase/firestore';
+import {
+    getByLimitedOrderQuery,
+    getByOrderedQuery,
+    getByQuery,
+    getCollectionReference,
+    getSingleDoc,
+} from '../server';
 import { getAllPhotoLikes } from '../views/likes';
 
 const collectionName = 'comments';
@@ -11,18 +18,26 @@ export const detailsPreloader = async (ctx, next) => {
     // Making the price to appear with 2 digits after decimal
     ctx.photo.price = Number(ctx.photo.price).toFixed(2);
 
-    ctx.photo.comments = await getByQuery(
+    ctx.photo.comments = await getByOrderedQuery(
         collectionReference,
         'photoId',
         '==',
-        ctx.params.id
+        ctx.params.id,
+        'createdAt',
+        'desc'
     );
 
+
+    console.log(ctx.photo.comments);
+
     ctx.photo.likes = (await getAllPhotoLikes(ctx)).length;
-    
+
     let likes = await getAllPhotoLikes(ctx);
-    ctx.photo.hasLiked = ctx.user ? likes.some((like) => like.userId == ctx.user.uid) : false;
-    
-    ctx.photo.showLikeButton = !ctx.photo.hasLiked && !ctx.photo.isOwner && ctx.hasUser
+    ctx.photo.hasLiked = ctx.user
+        ? likes.some((like) => like.userId == ctx.user.uid)
+        : false;
+
+    ctx.photo.showLikeButton =
+        !ctx.photo.hasLiked && !ctx.photo.isOwner && ctx.hasUser;
     next();
 };
